e6ee8bbafbeb6718c4283ec9a6e74da05370ad93
Maksim Pinigin <pinigin@mapicom.ru>
2019-03-30 14:23:25 +0700

    improved PHP-CGI support, POST requests is now supported

--- server.go
+++ server.go
@@ -34,7 +34,11 @@ func startServer(ip string, port int) {
 }
 
 func connectionHandler(conn net.Conn) {
-	var buffer string
+
+	var buffer string = ""
+	var firstLine string = ""
+	var contentlength int
+	var query string
 
 	reader := bufio.NewReader(conn)
 	for {
@@ -44,9 +48,14 @@ func connectionHandler(conn net.Conn) {
 			return
 		}
 
+		if firstLine == "" && buffer == "" {
+			firstLine = strings.Split(str, " ")[0]
+		}
+
 		buffer = buffer + str
-		if str == "\r\n" || str == "\n" {
-			lines := strings.Split(buffer, "\n")
+		if str == "\r\n" && firstLine != "POST" || str == "\n" && firstLine != "POST" {
+			strings.Trim(buffer, "\r")
+			lines := strings.Split(buffer, "\r\n")
 			for i := 0; i < len(lines); i++ {
 				if i == 0 {
 					if lines[i] == "" || lines[i] == "\r" {
@@ -57,9 +66,26 @@ func connectionHandler(conn net.Conn) {
 					params := strings.Split(lines[i], " ")
 					fmt.Printf("[INFO] [%s] %s - %s\n", conn.RemoteAddr().String(), params[0], params[1])
 
-					handleURL(conn, params[1])
+					handleURL(conn, params[0], params[1], lines, "")
 					break
 				}
+			}
+		} else if firstLine == "POST" {
+			if strings.Index(str, ": ") != -1 {
+				param := strings.SplitN(str, ": ", 2)
+				if param[0] == "Content-Length" {
+					contentlength, _ = strconv.Atoi(strings.Replace(param[1], "\r\n", "", -1))
+				}
+			}
+			if str == "\r\n" {
+				bytes := make([]byte, contentlength)
+				reader.Read(bytes)
+				query = string(bytes[:contentlength])
+				params := strings.Split(strings.Split(buffer, "\r\n")[0], " ")
+				lines := strings.Split(buffer, "\r\n")
+				fmt.Printf("[INFO] [%s] %s - %s\n", conn.RemoteAddr().String(), params[0], params[1])
+				handleURL(conn, params[0], params[1], lines, query)
+				break
 			}
 		}
 	}
@@ -108,8 +134,10 @@ func sendHTTPResponse(conn net.Conn, code int, contentType string, content inter
 	conn.Close()
 }
 
-func handleURL(conn net.Conn, urlp string) bool {
+func handleURL(conn net.Conn, method string, urlp string, all []string, query string) bool {
 	var url string
+	var urlWithQuestion string
+	urlWithQuestion = ""
 	//var origUrl string
 	if urlp[len(urlp)-1] == '/' {
 		url = urlp + defaultPage
@@ -119,6 +147,7 @@ func handleURL(conn net.Conn, urlp string) bool {
 
 	if questionIndex := strings.Index(url, "?"); questionIndex != -1 {
 		//origUrl = url
+		urlWithQuestion = url
 		url = url[:questionIndex]
 	}
 
@@ -151,7 +180,42 @@ func handleURL(conn net.Conn, urlp string) bool {
 	format := parseTwo[len(parseTwo)-1]
 
 	if format == "php" && phpCgi != "none" {
+		var params map[string]string
+		//var query string
+		params = make(map[string]string)
+		for i := 0; i<len(all); i++ {
+			if i == 0 {
+				continue
+			} else if all[i] == "" || all[i] == "\r\n" {
+				continue
+			} else if i == len(all)-1 {
+				break
+			}
+
+			splitted := strings.SplitN(all[i], ": ", 2)
+			params[splitted[0]] = splitted[1]
+		}
 		cmd := exec.Command(phpCgi)
+		cmd.Env = append(os.Environ(),
+			"REMOTE_ADDR=" + strings.Split(conn.RemoteAddr().String(), ":")[0],
+			"REMOTE_HOST=" + strings.Split(conn.RemoteAddr().String(), ":")[0],
+			"REQUEST_METHOD=" + method,
+			"SERVER_NAME=" + ip,
+			"SERVER_PORT=" + strconv.Itoa(port),
+			"SERVER_PROTOCOL=HTTP/1.1",
+			"SERVER_SOFTWARE=EHServ/0.1",
+			"CONTENT_LENGTH=" + params["Content-Length"],
+			"CONTENT_TYPE=" + params["Content-Type"],
+			"REDIRECT_STATUS=1",
+			"SCRIPT_FILENAME=" + webroot + url)
+
+		if urlWithQuestion != "" {
+			cmd.Env = append(cmd.Env, "QUERY_STRING=" + strings.SplitN(urlWithQuestion, "?", 2)[1])
+		}
+		if query != "" {
+			cmd.Env = append(cmd.Env, "QUERY_STRING=" + query)
+		}
+
 		stdin, _ := cmd.StdinPipe()
 		stdout, _ := cmd.StdoutPipe()
 		err = cmd.Start()
