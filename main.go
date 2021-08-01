ee63b7cce2fb082837a7c8417fbfe4fb83b34987
Maksim Pinigin <at@pinig.in>
2021-08-01 09:48:01 +0700

    Changed year in copyright

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	fmt.Println("EHServ 0.1 (c) 2019-2020, Maksim Pinigin")
+	fmt.Println("EHServ 0.1 (c) 2019-2021, Maksim Pinigin")
 	vHosts = make(map[string]string)
 	if loadConfig("ehserv.conf") == true {
 		startServer()
